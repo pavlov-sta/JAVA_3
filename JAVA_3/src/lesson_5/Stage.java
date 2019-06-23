3b0fb06e9f73dd31d37c4879bc013475bdfbf8d2
Stanislav <pavlov-sta@mail.ru>
2019-06-23 15:05:18 +0500

    ДЗ Гонка

--- JAVA_3/src/lesson_5/Stage.java
+++ JAVA_3/src/lesson_5/Stage.java
@@ -1,4 +1,12 @@
 package lesson_5;
 
-public class Stage {
+public abstract class Stage {
+    protected int length;
+    protected String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract void go(Car c);
 }
